667d9869953650b9d1af384e32a6a12467756370
Jordon Bedwell <envygeeks@gmail.com>
2013-05-08 21:14:53 -0500

    Coveralls

--- spec/rspec/helper.rb
+++ spec/rspec/helper.rb
@@ -1,11 +1,38 @@
 ENV["RAILS_ENV"], ENV["RACK_ENV"] = "test", "test"
 $:.unshift(File.expand_path("../../", __FILE__))
 
-require "simplecov"
-SimpleCov.start
+unless %W(no false).include?(ENV["COVERAGE"])
+  require "simplecov"
+  require "coveralls"
+
+  module Coveralls
+    NoiseBlacklist = [
+      "[Coveralls] Using SimpleCov's default settings.".green,
+      "[Coveralls] Set up the SimpleCov formatter.".green,
+      "[Coveralls] Outside the Travis environment, not sending data.".yellow
+    ]
+
+    def puts(message)
+      # Only prevent the useless noise on our terminals, not inside of the Travis or Circle CI.
+      unless NoiseBlacklist.include?(message) || ENV["TRAVIS"] == "true" || ENV["CI"] == "true"
+        $stdout.puts message
+      end
+    end
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+        Coveralls::SimpleCov::Formatter
+  ]
+
+  Coveralls.noisy = true
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+end
 
 require "mongoid/finders/find_by"
-Mongoid.load!(File.expand_path("../../config/mongoid.yml", __FILE__), :test)
+
 module Mongoid::SpecHelpers
   class << self
     def random_class
@@ -39,5 +66,12 @@ end
 RSpec.configure do |config|
   config.before(:each) do
     Mongoid.purge!
+    Mongoid::IdentityMap.clear
+  end
+
+  config.after(:suite) do
+    Mongoid::Threaded.sessions[:default].drop
   end
 end
+
+Mongoid.load!(File.expand_path("../../config/mongoid.yml", __FILE__), :test)
